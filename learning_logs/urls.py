8e0725084081c291c41fcd63c1e4726770b6bf33
username <123misha.stetsyuk@gmail.com>
2022-08-03 05:12:23 +0300

    Added edit entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -17,5 +17,8 @@ urlpatterns = [
     path('new_topic/',views.new_topic,name='new_topic'),
     # Сторінка для додавання нових статтей
     path("new_entry/<int:topic_id>/",views.new_entry,name='new_entry'),
+    # Сторінка для редагування дописів
+    path('edit_entry/<int:entry_id>/',views.edit_entry,name='edit_entry'),
+
 
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -53,7 +53,25 @@ def new_entry(request, topic_id):
             new_entry = form.save(commit=False)
             new_entry.topic = topic
             new_entry.save()
-            return redirect('learning_logs:topic',topic_id=topic_id)
+            return redirect('learning_logs:topic', topic_id=topic_id)
         # Показати порожню або недійсну форму
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request...
+        form = EntryForm(instance=entry)
+    else:
+        # Post data submited
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
